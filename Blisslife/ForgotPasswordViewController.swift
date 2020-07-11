aff74ebb95e3531acd98fb646f6ac526a8607c82
Likhit <likhitgmella@gmail.com>
2020-07-12 00:12:43 +0530

    forgot password screen done

---
+++ Blisslife/ForgotPasswordViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ForgotPasswordViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 12/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ForgotPasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
