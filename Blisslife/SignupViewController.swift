4d3c8169d691a6f23a6f59e43f229dfabe740d20
Likhit <likhitgmella@gmail.com>
2020-07-12 14:01:40 +0530

    signup screen setup & file setup

---
+++ Blisslife/SignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignupViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 12/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
