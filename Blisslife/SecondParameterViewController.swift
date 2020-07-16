750b73305eef17f12f69b76ac43f2a5c2f6cfe88
Likhit <likhitgmella@gmail.com>
2020-07-16 23:23:43 +0530

    second & third param screens & VCs setup

---
+++ Blisslife/SecondParameterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondParameterViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 16/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SecondParameterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Blisslife/ThirdParameterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ThirdParameterViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 16/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ThirdParameterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
