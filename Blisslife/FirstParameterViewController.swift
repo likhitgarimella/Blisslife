4f390ed968886738a7804c3d5a4ddd55b8446373
Likhit <likhitgmella@gmail.com>
2020-07-17 02:57:14 +0530

    first param VC outlets & prop

--- Blisslife/FirstParameterViewController.swift
+++ Blisslife/FirstParameterViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class FirstParameterViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var lowerRange: UITextField!
+    @IBOutlet var upperRange: UITextField!
+    
+    func Properties() {
+        
+        lowerRange.layer.cornerRadius = 8
+        lowerRange.layer.borderWidth = 1
+        lowerRange.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
+        upperRange.layer.cornerRadius = 8
+        upperRange.layer.borderWidth = 1
+        upperRange.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        hideKeyboardWhenTappedAround()
+        Properties()
         
     }
     
-}   // #21
+}   // #37
