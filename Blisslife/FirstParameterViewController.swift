96a3dc5b6eca8885553eb8654ea565365318004f
Likhit <likhitgmella@gmail.com>
2020-07-16 23:19:23 +0530

    first param VC setup done

---
+++ Blisslife/FirstParameterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstParameterViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 16/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class FirstParameterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
