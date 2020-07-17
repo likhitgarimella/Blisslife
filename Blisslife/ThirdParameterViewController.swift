aa5111e2f93f2ee07b57748f77d9c4b3cd64dfb5
Likhit <likhitgmella@gmail.com>
2020-07-17 22:38:35 +0530

    third param screen all button outlets & actions

--- Blisslife/ThirdParameterViewController.swift
+++ Blisslife/ThirdParameterViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ThirdParameterViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var alcohol: UIButton!
+    @IBOutlet var tobacco: UIButton!
+    @IBOutlet var drugs: UIButton!
+    @IBOutlet var nota: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +23,40 @@ class ThirdParameterViewController: UIViewController {
         
     }
     
-}   // #21
+    @IBAction func alcoholAction(_ sender: UIButton) {
+        
+        alcohol.backgroundColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        tobacco.backgroundColor = UIColor.white
+        drugs.backgroundColor = UIColor.white
+        nota.backgroundColor = UIColor.white
+        
+    }
+    
+    @IBAction func tobaccoAction(_ sender: UIButton) {
+        
+        alcohol.backgroundColor = UIColor.white
+        tobacco.backgroundColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        drugs.backgroundColor = UIColor.white
+        nota.backgroundColor = UIColor.white
+        
+    }
+    
+    @IBAction func drugsAction(_ sender: UIButton) {
+        
+        alcohol.backgroundColor = UIColor.white
+        tobacco.backgroundColor = UIColor.white
+        drugs.backgroundColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        nota.backgroundColor = UIColor.white
+        
+    }
+    
+    @IBAction func notaAction(_ sender: UIButton) {
+        
+        alcohol.backgroundColor = UIColor.white
+        tobacco.backgroundColor = UIColor.white
+        drugs.backgroundColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        nota.backgroundColor = UIColor.white
+        
+    }
+    
+}   // #63
