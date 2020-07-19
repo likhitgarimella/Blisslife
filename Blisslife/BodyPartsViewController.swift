b09cd7a539977d3ce204c6b26d56801fded693b8
Likhit <likhitgmella@gmail.com>
2020-07-20 02:29:37 +0530

    corresponding 4 VC files & hierarchy

---
+++ Blisslife/BodyPartsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BodyPartsViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 20/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class BodyPartsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Blisslife/SympsCondsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SympsCondsViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 20/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SympsCondsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
