5506f03384c1545951fa9c8030019ab797b7ac89
Likhit <likhitgmella@gmail.com>
2020-07-20 22:21:49 +0530

    all 12 buttons properties

--- Blisslife/HealthGoalsViewController.swift
+++ Blisslife/HealthGoalsViewController.swift
@@ -23,6 +23,36 @@ class HealthGoalsViewController: UIViewController {
     @IBOutlet var ten: UIButton!
     @IBOutlet var eleven: UIButton!
     @IBOutlet var twelve: UIButton!
+    
+    func Properties() {
+        
+        one.backgroundColor = UIColor.white
+        two.backgroundColor = UIColor.white
+        three.backgroundColor = UIColor.white
+        four.backgroundColor = UIColor.white
+        five.backgroundColor = UIColor.white
+        six.backgroundColor = UIColor.white
+        seven.backgroundColor = UIColor.white
+        eight.backgroundColor = UIColor.white
+        nine.backgroundColor = UIColor.white
+        ten.backgroundColor = UIColor.white
+        eleven.backgroundColor = UIColor.white
+        twelve.backgroundColor = UIColor.white
+        
+        one.layer.cornerRadius = 10
+        two.layer.cornerRadius = 10
+        three.layer.cornerRadius = 10
+        four.layer.cornerRadius = 10
+        five.layer.cornerRadius = 10
+        six.layer.cornerRadius = 10
+        seven.layer.cornerRadius = 10
+        eight.layer.cornerRadius = 10
+        nine.layer.cornerRadius = 10
+        ten.layer.cornerRadius = 10
+        eleven.layer.cornerRadius = 10
+        twelve.layer.cornerRadius = 10
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +60,8 @@ class HealthGoalsViewController: UIViewController {
         // remove title for left bar button item
         navigationController?.navigationBar.topItem?.title = ""
         
+        Properties()
+        
     }
     
-}   // #36
+}   // #68
