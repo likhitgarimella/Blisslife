9a8c85426ecee96840c6299a3dce74824ea39ca9
Likhit <likhitgmella@gmail.com>
2020-07-23 14:08:30 +0530

    main VC file setup

---
+++ Blisslife/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 23/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
