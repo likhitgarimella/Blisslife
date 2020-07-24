495e64f68b9b7ce33c1b420421206c8231a0f65e
Likhit <likhitgmella@gmail.com>
2020-07-24 09:49:37 +0530

    ~food~ top bar, card done

--- Blisslife/FoodViewController.swift
+++ Blisslife/FoodViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class FoodViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var topBar: UIView!
+    @IBOutlet var recentRecipeCard: UIView!
+    
+    func TopBar() {
+        
+        // TopBar UIView Properties
+        topBar.layer.masksToBounds = false
+        topBar.layer.shadowRadius = 1
+        topBar.layer.shadowOpacity = 0.2
+        topBar.layer.shadowColor = UIColor.gray.cgColor
+        topBar.layer.shadowOffset = CGSize(width: 0, height: 2)
+        
+    }
+    
+    func RecentRecipeCardProp() {
+        
+        recentRecipeCard.layer.cornerRadius = 6
+        recentRecipeCard.layer.masksToBounds = false
+        recentRecipeCard.layer.shadowRadius = 1
+        recentRecipeCard.layer.shadowOpacity = 0.4
+        recentRecipeCard.layer.shadowColor = UIColor.gray.cgColor
+        recentRecipeCard.layer.shadowOffset = CGSize(width: 0, height: 1)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        TopBar()
+        RecentRecipeCardProp()
         
     }
     
-}   // #21
+}   // #48
