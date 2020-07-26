253d6259e1c8dbeebec77b51a2841a61d9f1d9e1
Likhit <likhitgmella@gmail.com>
2020-07-27 01:09:31 +0530

    recipe model class setup

---
+++ Blisslife/RecipeModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeModel.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 27/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class RecipeModel: NSObject {
+    
+}   // #14
