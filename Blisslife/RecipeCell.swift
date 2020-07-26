b0e621bc57d4d982213cd3be97017cbdb598b919
Likhit <likhitgmella@gmail.com>
2020-07-26 22:03:46 +0530

    recipe coll view & cell setup

---
+++ Blisslife/RecipeCell.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeCell.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 26/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UICollectionViewCell {
+    
+}   // #14
