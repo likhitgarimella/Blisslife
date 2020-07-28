3c6c27784fcc914fd83df97958fc4f5cf4d9dd03
Likhit <likhitgmella@gmail.com>
2020-07-28 21:19:15 +0530

    detail VC - recipe cell labels & props

--- Section-2 Food Section/DetailViewController.swift
+++ Section-2 Food Section/DetailViewController.swift
@@ -15,6 +15,29 @@ class DetailViewController: UIViewController {
     
     // Outlets
     @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet var one: UILabel!
+    @IBOutlet var two: UILabel!
+    @IBOutlet var three: UILabel!
+    
+    func Properties() {
+        
+        one.clipsToBounds = true
+        one.layer.cornerRadius = 12
+        one.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
+        one.layer.borderWidth = 1.2
+        
+        two.clipsToBounds = true
+        two.layer.cornerRadius = 12
+        two.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
+        two.layer.borderWidth = 1.2
+        
+        three.clipsToBounds = true
+        three.layer.cornerRadius = 12
+        three.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
+        three.layer.borderWidth = 1.2
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +51,8 @@ class DetailViewController: UIViewController {
         // set image
         imageView.image = UIImage(named: dataModel.imageName)
         
+        Properties()
+        
     }
     
-}   // #34
+}   // #59
