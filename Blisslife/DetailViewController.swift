8b171c78031a6d6fde6b5c4df71ccae449d7569a
Likhit <likhitgmella@gmail.com>
2020-07-28 05:31:03 +0530

    detail VC & storyboard setup

---
+++ Blisslife/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 28/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
