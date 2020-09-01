b28548d36b4c8e78a2f0fa50720d311ec1f10957
Likhit <likhitgmella@gmail.com>
2020-09-02 00:10:36 +0530

    see all recipe coll view

---
+++ Section-2 Food Section/SeeAllRecipeCollections.swift
@@ -0,0 +1,41 @@
+//
+//  SeeAllRecipeCollections.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 02/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class SeeAllRecipeCollections: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // Outlets
+    @IBOutlet var recipesCollView2: UICollectionView!
+    
+    /// images in food diet list coll view
+    var items1 = [UIImage(named: "RecipeCard3"), UIImage(named: "RecipeCard4"), UIImage(named: "RecipeCard3"), UIImage(named: "RecipeCard4"), UIImage(named: "RecipeCard3"), UIImage(named: "RecipeCard4"), UIImage(named: "RecipeCard3"), UIImage(named: "RecipeCard4"), UIImage(named: "RecipeCard3"), UIImage(named: "RecipeCard4")]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // nav bar color
+        // navigationController?.navigationBar.barTintColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
+        
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items1.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "RecipeCollectionListCell", for: indexPath as IndexPath) as! RecipeCollectionListCell
+        cell1.backgroundColor = UIColor.white
+        cell1.dietImage.image = items1[indexPath.row]
+        return cell1
+    }
+    
+}   // #42
