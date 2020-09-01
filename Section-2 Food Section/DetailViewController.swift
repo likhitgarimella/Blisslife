b73e65e4d218f3e23409fef0c621e4cb10716c61
Likhit <likhitgmella@gmail.com>
2020-09-01 17:32:30 +0530

    detail VC code delete

--- Section-2 Food Section/DetailViewController.swift
+++ Section-2 Food Section/DetailViewController.swift
@@ -10,40 +10,6 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    // from prev controller
-    var dataModel: RecipeModel!
-    
-    // Outlets
-    @IBOutlet weak var imageView: UIImageView!
-    
-    @IBOutlet var one: UILabel!
-    @IBOutlet var two: UILabel!
-    @IBOutlet var three: UILabel!
-    
-    @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var personLabel: UILabel!
-    @IBOutlet var timeLabel: UILabel!
-    @IBOutlet var peopleLabel: UILabel!
-    
-    func Properties() {
-        
-        one.clipsToBounds = true
-        one.layer.cornerRadius = 12
-        one.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
-        one.layer.borderWidth = 1.2
-        
-        two.clipsToBounds = true
-        two.layer.cornerRadius = 12
-        two.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
-        two.layer.borderWidth = 1.2
-        
-        three.clipsToBounds = true
-        three.layer.cornerRadius = 12
-        three.layer.borderColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0).cgColor
-        three.layer.borderWidth = 1.2
-        
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -53,23 +19,6 @@ class DetailViewController: UIViewController {
         // remove title for left bar button item
         navigationController?.navigationBar.topItem?.title = ""
         
-        Properties()
-        
-        // set image
-        imageView.image = UIImage(named: dataModel.imageName)
-        
-        // set title label
-        titleLabel.text = dataModel.recipeTitle
-        
-        // set person label
-        personLabel.text = dataModel.personName
-        
-        // set time label
-        timeLabel.text = dataModel.timeDur
-        
-        // set people label
-        peopleLabel.text = dataModel.peopleNumber
-        
     }
     
-}   // #76
+}   // #25
