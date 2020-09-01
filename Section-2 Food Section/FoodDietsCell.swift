3fcd5819e3f44283eeec34889690c1adcf3b0a5d
Likhit <likhitgmella@gmail.com>
2020-09-01 18:32:34 +0530

    food diets coll view

---
+++ Section-2 Food Section/FoodDietsCell.swift
@@ -0,0 +1,15 @@
+//
+//  FoodDietsCell.swift
+//  Blisslife
+//
+//  Created by Likhit Garimella on 01/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class FoodDietsCell: UICollectionViewCell {
+    
+    @IBOutlet var dietImage: UIImageView!
+    
+}   // #16
--- Section-2 Food Section/FoodViewController.swift
+++ Section-2 Food Section/FoodViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class FoodViewController: UIViewController {
+class FoodViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // Outlets
+    @IBOutlet var foodDietsCollView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +19,14 @@ class FoodViewController: UIViewController {
         // nav bar color
         // navigationController?.navigationBar.barTintColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
         
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
     
 }   // #22
