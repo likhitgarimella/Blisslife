c7d5bc18d06d8be848db4d6297b650b5a8974ced
Likhit <likhitgmella@gmail.com>
2020-09-03 00:42:52 +0530

    recipes for you cell done

--- Section-2 Food Section/RecipesForYouCell.swift
+++ Section-2 Food Section/RecipesForYouCell.swift
@@ -10,4 +10,21 @@ import UIKit
 
 class RecipesForYouCell: UICollectionViewCell {
     
-}   // #14
+    // Outlets
+    @IBOutlet var card: UIView!
+    
+    @IBOutlet var recipeImage: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var byPerson: UILabel!
+    @IBOutlet var time: UILabel!
+    @IBOutlet var people: UILabel!
+    
+    override func awakeFromNib() {
+        card.layer.shadowColor = UIColor.darkGray.cgColor
+        card.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        card.layer.shadowRadius = 2.0
+        card.layer.shadowOpacity = 0.5
+        card.layer.masksToBounds = false
+    }
+    
+}   // #31
