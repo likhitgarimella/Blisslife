02d336614f84a896e3a4d38983b947546a157b8f
Likhit <likhitgmella@gmail.com>
2020-09-03 01:20:45 +0530

    details vc done

--- Section-2 Food Section/DetailsViewController.swift
+++ Section-2 Food Section/DetailsViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var likesImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // nav bar color
+        // navigationController?.navigationBar.barTintColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
         
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
+        
+        let tapGestureForLikeImageView = UITapGestureRecognizer(target: self, action: #selector(likeImageViewTouch))
+        likesImageView.addGestureRecognizer(tapGestureForLikeImageView)
+        likesImageView.isUserInteractionEnabled = true
         
     }
     
-}   // #21
+    @objc func likeImageViewTouch() {
+        
+        likesImageView.image = UIImage(named: "likeOn")
+        
+    }
+    
+}   // #38
