0bc51becf503251089f7f5c18c6995be97373f2c
Likhit <likhitgmella@gmail.com>
2020-09-02 18:55:50 +0530

    pop rec data popln

--- Section-2 Food Section/FoodViewController.swift
+++ Section-2 Food Section/FoodViewController.swift
@@ -27,6 +27,14 @@ class FoodViewController: UIViewController, UICollectionViewDelegate, UICollecti
     /// images in popular recipe coll view
     var items4 = [UIImage(named: "keto"), UIImage(named: "juice"), UIImage(named: "vegans"), UIImage(named: "keto"), UIImage(named: "juice"), UIImage(named: "vegans"), UIImage(named: "keto"), UIImage(named: "juice"), UIImage(named: "vegans")]
     
+    var titles = ["Quinoa Salad", "Celery Soup", "Smoothie Bowl", "Quinoa Salad", "Celery Soup", "Smoothie Bowl", "Quinoa Salad", "Celery Soup", "Smoothie Bowl"]
+    
+    var byPersons = ["by Kristen Stewart", "by Naomi Scott", "by Ella Balinska", "by Kristen Stewart", "by Naomi Scott", "by Ella Balinska", "by Kristen Stewart", "by Naomi Scott", "by Ella Balinska"]
+    
+    var times = ["10 mins", "20 mins", "30 mins", "10 mins", "20 mins", "30 mins", "10 mins", "20 mins", "30 mins"]
+    
+    var people = ["1 people", "2 people", "3 people", "1 people", "2 people", "3 people", "1 people", "2 people", "3 people"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -39,7 +47,10 @@ class FoodViewController: UIViewController, UICollectionViewDelegate, UICollecti
         if collectionView == self.foodDietsCollView {
             return items1.count
         }
-        return items2.count
+        if collectionView == self.recipesCollView {
+            return items2.count
+        }
+        return items3.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -59,8 +70,12 @@ class FoodViewController: UIViewController, UICollectionViewDelegate, UICollecti
             let cell3 = collectionView.dequeueReusableCell(withReuseIdentifier: "PopularRecipeCell", for: indexPath as IndexPath) as! PopularRecipeCell
             cell3.backgroundColor = UIColor.white
             cell3.recipeImage.image = items2[indexPath.row]
+            cell3.title.text = titles[indexPath.item]
+            cell3.byPerson.text = byPersons[indexPath.item]
+            cell3.time.text = times[indexPath.item]
+            cell3.people.text = people[indexPath.item]
             return cell3
         }
     }
     
-}   // #57
+}   // #82
