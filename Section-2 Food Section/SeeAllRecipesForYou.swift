854bd8e26e70ad37a2e4a25425a4913f8ebe92fa
Likhit <likhitgmella@gmail.com>
2020-09-03 01:14:45 +0530

    see all rec for you data popln

--- Section-2 Food Section/SeeAllRecipesForYou.swift
+++ Section-2 Food Section/SeeAllRecipesForYou.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class SeeAllRecipesForYou: UIViewController {
-
+class SeeAllRecipesForYou: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // Outlets
+    @IBOutlet var recipesForYouCollView2: UICollectionView!
+    
+    /// images in popular recipe coll view
+    var items1 = [UIImage(named: "vegans"), UIImage(named: "juice"), UIImage(named: "keto"), UIImage(named: "vegans"), UIImage(named: "juice"), UIImage(named: "keto"), UIImage(named: "vegans"), UIImage(named: "juice"), UIImage(named: "keto")]
+    
+    var titles = ["Quinoa Salad", "Celery Soup", "Smoothie Bowl", "Quinoa Salad", "Celery Soup", "Smoothie Bowl", "Quinoa Salad", "Celery Soup", "Smoothie Bowl"]
+    
+    var byPersons = ["by Kristen Stewart", "by Naomi Scott", "by Ella Balinska", "by Kristen Stewart", "by Naomi Scott", "by Ella Balinska", "by Kristen Stewart", "by Naomi Scott", "by Ella Balinska"]
+    
+    var times = ["10 mins", "20 mins", "30 mins", "10 mins", "20 mins", "30 mins", "10 mins", "20 mins", "30 mins"]
+    
+    var people = ["1 people", "2 people", "3 people", "1 people", "2 people", "3 people", "1 people", "2 people", "3 people"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // nav bar color
+        // navigationController?.navigationBar.barTintColor = UIColor(red: 251.0/255.0, green: 215.0/255.0, blue: 69.0/255.0, alpha: 1.0)
         
+        // remove title for left bar button item
+        navigationController?.navigationBar.topItem?.title = ""
         
     }
     
